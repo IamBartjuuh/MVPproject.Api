b008305f4547f9e28505f86ea7d675712512c47a
IamBartjuuh <bartatgier@hotmail.com>
2025-03-11 15:39:55 +0100

    Start Unit Tests
    Not implemented just syncing with pc

---
+++ ProjectNaam.WebApi.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using ProjectNaam.WebApi.Controllers;
+
+namespace ProjectNaam.WebApi.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
